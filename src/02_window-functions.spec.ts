db89bcd60d2a1b3d76c56a8d57e7f62325a56d61
Grant Powell <grant@cotera.co>
2024-02-16 22:51:53 -0500

    Get the basic setup of the specs in

---
+++ src/02_window-functions.spec.ts
@@ -0,0 +1,35 @@
+import { Asc, Values } from "@cotera/nasty";
+import { db, CHANGE_ME } from "./helpers";
+import { describe, test, expect } from "bun:test";
+
+describe("02_window-functions", () => {
+	// Section Goals
+	//
+	// 1. Use Rank
+	// 2. Use Lead/Lag
+	// 3. Partition By
+	test.skip("Lead/Lag", async () => {
+		const data = [{ a: 1 }, { a: 2 }, { a: 3 }, { a: 4 }];
+
+		const query = Values(data)
+			.select((t) => {
+				const a = t.attr("a");
+
+				return {
+					a,
+					lead: CHANGE_ME(),
+					lag: CHANGE_ME(),
+				};
+			})
+			.sort((t) => Asc(t.attr("a")));
+
+		expect(await query.execute(db())).toEqual([
+			{ a: 1, lead: 2, lag: null },
+			{ a: 2, lead: 3, lag: 1 },
+			{ a: 3, lead: 4, lag: 2 },
+			{ a: 4, lead: null, lag: 3 },
+		]);
+	});
+
+	test.skip("partiton by", () => {});
+});
---
+++ src/helpers.ts
@@ -0,0 +1,12 @@
+import { DuckDbNativeDriver } from "@cotera/nasty-drivers";
+
+export const CHANGE_ME = () => {
+	const err = new Error("CHANGE ME");
+	if ((Error as any).captureStackTrace) {
+		(Error as any).captureStackTrace(err, CHANGE_ME);
+	}
+
+	throw err;
+};
+
+export const db = () => DuckDbNativeDriver.emphemeral();
