e0304443f3f8dc6495068222138bea110e4cefad
Kendra Bobby <kendra@cotera.co>
2024-02-20 17:11:51 -0500

    start aggregate specs and create hockey data

--- src/03_aggregates.spec.ts
+++ src/03_aggregates.spec.ts
@@ -1,10 +1,40 @@
+import {
+  Avg,
+  Count,
+  CountDistinct,
+  Max,
+  Median,
+  Min,
+  Sum,
+  Values,
+} from "@cotera/nasty";
+import { HOCKEY_DATA } from "./datasets/hockey";
+import { db } from "./helpers";
+
 describe("Aggregates", () => {
-	describe("summary", () => {
-		// Section Goals
-		//
-		// 0. Learn how to group by no attributes
-		// 1. Learn how to use `.summary` shorthand
-		//
-		test.skip("grouping on no attributes", () => {});
-	});
+  describe("summary", () => {
+    test("summary", async () => {
+      const query = Values(HOCKEY_DATA).summary((t) => {
+        return {
+          TeamCount: Count(),
+          ConferenceCount: CountDistinct(t.attr("conference")),
+          TotalWins: Sum(t.attr("wins")),
+          AvgWins: Avg(t.attr("wins")),
+          MinWins: Min(t.attr("wins")),
+          MaxWins: Max(t.attr("wins")),
+          MedianWins: Median(t.attr("wins")),
+        };
+      });
+      expect(await query.execute(db())).toEqual([
+        { AvgWins: 2, count: 3, max: 3, min: 1, sum: 6 },
+      ]);
+    });
+
+    // Section Goals
+    //
+    // 0. Learn how to group by no attributes
+    // 1. Learn how to use `.summary` shorthand
+    //
+    test.skip("grouping on no attributes", () => {});
+  });
 });
---
+++ src/datasets/hockey.ts
@@ -0,0 +1,50 @@
+export const HOCKEY_DATA = [
+  {
+    team: "Bruins",
+    conference: "Eastern",
+    division: "Atlantic",
+    wins: 44,
+  },
+  {
+    team: "Maple Leafs",
+    conference: "Eastern",
+    division: "Atlantic",
+    wins: 40,
+  },
+  {
+    team: "Capitals",
+    conference: "Eastern",
+    division: "Metropolitan",
+    wins: 49,
+  },
+  {
+    team: "Rangers",
+    conference: "Eastern",
+    division: "Metropolitan",
+    wins: 34,
+  },
+  {
+    team: "Predators",
+    conference: "Western",
+    division: "Central",
+    wins: 53,
+  },
+  {
+    team: "Wild",
+    conference: "Western",
+    division: "Central",
+    wins: 45,
+  },
+  {
+    team: "Oilers",
+    conference: "Western",
+    division: "Pacific",
+    wins: 47,
+  },
+  {
+    team: "Canucks",
+    conference: "Western",
+    division: "Pacific",
+    wins: 35,
+  },
+];
