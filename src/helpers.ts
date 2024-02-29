841608746b2008afb0ef8da164380dd5473664a2
Grant Powell <grant@cotera.co>
2024-02-29 14:00:56 -0500

    remove nasty-drivers

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,7 @@
-import { DuckDbNativeDriver } from "@cotera/nasty-drivers";
+import { Relation, Ty } from "@cotera/nasty";
+import { PGlite } from "@electric-sql/pglite";
+
+const pg = new PGlite();
 
 export const CHANGE_ME = () => {
 	const err = new Error("CHANGE ME");
@@ -8,4 +11,15 @@ export const CHANGE_ME = () => {
 	throw err;
 };
 
-export const db = () => DuckDbNativeDriver.emphemeral();
+class TestDb {
+	async execute(relation: Relation): Promise<Record<string, Ty.Literal>[]> {
+		const { sql, params } = relation.postgresSql;
+		if (params.length > 0) {
+			throw new Error(`TODO: Deal with parameters ${params}`);
+		}
+		const res = pg.query(sql);
+		return res as any;
+	}
+}
+
+export const db = () => new TestDb();
