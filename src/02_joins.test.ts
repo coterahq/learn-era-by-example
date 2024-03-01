9af91eee2176097fb42021c92f3ab60e277f2725
Grant Powell <grant@cotera.co>
2024-03-01 13:44:58 -0500

    Move joins into their own lesson

---
+++ src/02_joins.test.ts
@@ -0,0 +1,40 @@
+import { Values } from "@cotera/nasty";
+import { db, CHANGE_ME } from "./helpers";
+import { describe, test, expect } from "vitest";
+
+describe("join", () => {
+  // Section goals
+  //
+  // 0. Inner Join
+  // 2. Left Join
+  // 3. Right Join
+  // 4. Deal with attributes that are in both sides of the join
+
+  const Users = Values([
+    { id: 1, name: "Kendra" },
+    { id: 2, name: "TJ" },
+    { id: 3, name: "Ibby" },
+    { id: 4, name: "Tom" },
+    { id: 5, name: "Grant" },
+  ]);
+
+  const Preferences = Values([
+    { user_id: 1, fav_color: "blue" },
+    { user_id: 3, fav_color: "red" },
+  ]);
+
+  test.skip("inner join two tables", async () => {
+    const query = Users.leftJoin(Preferences, (user, preference) => ({
+      on: CHANGE_ME(),
+      select: {
+        ...user.star(),
+        ...preference.except("user_id"),
+      },
+    }));
+
+    expect(await query.execute(db())).toEqual([
+      { fav_color: "blue", id: 1, name: "Kendra" },
+      { fav_color: "red", id: 3, name: "Ibby" },
+    ]);
+  });
+});
--- src/helpers.ts
+++ src/helpers.ts
@@ -1,6 +1,6 @@
 import { DuckDbNativeDriver } from "@cotera/nasty-drivers";
 
-export const CHANGE_ME = () => {
+export const CHANGE_ME = (..._vars: any[]) => {
   const err = new Error("CHANGE ME");
   if ((Error as any).captureStackTrace) {
     (Error as any).captureStackTrace(err, CHANGE_ME);
